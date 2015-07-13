4404a6138bcee154d46a2c2d7318e20c74ca51b1
maiah <maiah@burstsms.com>
2015-07-13 16:11:38 +0800

    Implemented add and minus lines

--- main.go
+++ main.go
@@ -3,12 +3,47 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 
-	for i := 0; i <= 200; i++ {
-		fmt.Println()
+	lines := 200
+
+	if len(os.Args) > 1 {
+		arg := os.Args[1:][0]
+
+		if len(arg) > 1 {
+			firstChar := string([]rune(arg)[0])
+			count, err := strconv.Atoi(arg[1:len(arg)])
+
+			if err != nil {
+				panic(err)
+			}
+
+			switch firstChar {
+			case "-":
+				lines -= count
+			case "+":
+				lines += count
+			default:
+				lines, err = strconv.Atoi(arg)
+				if err != nil {
+					panic(err)
+				}
+			}
+
+		} else {
+			count, err := strconv.Atoi(arg)
+			if err != nil {
+				panic(err)
+			}
+
+			lines = count
+		}
 	}
 
+	for i := 0; i < lines; i++ {
+		fmt.Println()
+	}
 }
